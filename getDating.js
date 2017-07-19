753f61a653d41c4f8a1300e5d74655c8dd42d951
kanata <wo.ak.n7.kt.nah@gmail.com>
2017-07-19 16:53:23 +0900

    V0.2

--- getDating.js
+++ getDating.js
@@ -1,5 +1,6 @@
-function getDating(){
+function getDating(inputDate){
   var date = new Date();
+  date.setDate(date.getDate() + inputDate);
   var monthsE = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var dayOfWeeksJ = ["日","月","火","水","木","金","土"];
   var dayOfWeeksE = ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
--- main.js
+++ main.js
@@ -1,26 +1,49 @@
 var constWord = ['year', 'month', 'monthE', 'monthE3', 'date', 'dayJ', 'dayE', 'dayE3', 'dayJF'];
 var clicked = false;
+var dateMode = 0;
 document.body.addEventListener('click', function(e){
   if(e.target.parentNode.className === 'page-link'){
     e.preventDefault();
     if(clicked){
       clicked = false;
       var projectName = decodeURIComponent(e.target.parentNode.href).split("/")[3];
-      var nextPageTitle = decodeURIComponent(e.target.parentNode.href).split("/")[4];
+      decodeURIComponent(e.target.parentNode.href).split("/")[4];
       var request = new XMLHttpRequest();
       request.open("GET", "https://scrapbox.io/api/pages/" + projectName + "/" + nextPageTitle + "/text", true);
-      request.onload = function(r){
-        if(request.readyState === 4) {
-          if(request.status === 200) {
+      request.onload = function (r){
+        if (request.readyState === 4) {
+          if (request.status === 200) {
             templateText = request.responseText.split(/\r\n|\r|\n/);
             if(templateText[1] === '#TemplateLanguage'){
-              if(templateText.length > 4){
-                if(templateText[4] === ''){
+              if(templateText.length > 5){
+                var dates;
+                var regexp = /d[0-9]/;
+                if(!regexp.test(templateText[2])){
+                  window.confirm('error:日付変数オプションが設定されてません.');
+                  return;
+                }
+                if(templateText[2] === 'd1'){
+                  dateMode = 1;
+                  dates = getDating(0);
+                }
+                else if(templateText[2] === 'd2'){
+                  dateMode = 2;
+                  var dateMessage = window.prompt('今日を起点(0)としてずらしたい日数分の数字(半角入力)を入力してください.(明日なら 1, 明後日なら 2, 昨日なら -1, 一昨日なら -2)', 0);
+                  dateMessage = dateMessage !== null ? dateMessage : 0;
+                  dates = getDating(-(-dateMessage));
+                }
+                else if(templateText[2] === 'd3'){
+                  dateMode = 3;
+                  var dateMessage = window.prompt('今日を起点(0)としてずらしたい日数分の数字(半角入力)を入力してください.(昨日なら 1, 一昨日なら 2, 明日なら -1, 明後日なら -2)', 0);
+                  dateMessage = dateMessage !== null ? dateMessage : 0;
+                  dates = getDating(-dateMessage);
+                }
+                if(templateText[5] === ''){
                   window.confirm('error:ページ名が存在しません.');
                   return;
                 }
-                if(templateText[2] > 0){
-                  var inputQ = templateText[3].split(/\$/);
+                if(templateText[3] > 0){
+                  var inputQ = templateText[4].split(/\$/);
                   var promptMessage = [];
                   for(var i = 0; i < inputQ.length; i++){
                     var promptText = '次の内容を入力してください.\n' + inputQ[i];
@@ -28,26 +51,26 @@ document.body.addEventListener('click', function(e){
                     promptMessage[i] = message !== null ? message : '';
                   }
                 }
-                var dates = getDating();
-                var regexp;
                 var bodyContents = '';
-                for(var i = 4; i < templateText.length; i++){
-                  for(var j = 0; j < constWord.length; j++){
-                    regexp = new RegExp('\\(\\$' + constWord[j] + '\\)', 'g');
-                    templateText[i] = templateText[i].replace(regexp, dates[constWord[j]]);
+                for(var i = 5; i < templateText.length; i++){
+                  if(dateMode != 0){
+                    for(var j = 0; j < constWord.length; j++){
+                      regexp = new RegExp('\\(\\$' + constWord[j] + '\\)', 'g');
+                      templateText[i] = templateText[i].replace(regexp, dates[constWord[j]]);
+                    }
                   }
-                  for(j = 0; j < templateText[2]; j++){
+                  for(j = 0; j < templateText[3]; j++){
                     regexp = new RegExp('\\(\\$' + (j + 1) + '\\)', 'g');
                     templateText[i] = templateText[i].replace(regexp, promptMessage[j]);
                   }
-                  if(i == 5){
+                  if(i == 6){
                     bodyContents = templateText[i];
                   }
-                  else if(i != 4){
+                  else if(i != 5){
                     bodyContents = bodyContents + '\n' + templateText[i];
                   }
                 }
-                location.href = 'https://scrapbox.io/' + encodeURIComponent(projectName) + '/' + encodeURIComponent(templateText[4]) + '?body=' + encodeURIComponent(bodyContents);
+                location.href = 'https://scrapbox.io/' + encodeURIComponent(projectName) + '/' + encodeURIComponent(templateText[5]) + '?body=' + encodeURIComponent(bodyContents);
               }
               else{
                 window.confirm('error:テンプレートページがルールに従っていません.');
